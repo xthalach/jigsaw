fb06df95324b4a0b0c9c28875b7ac21dacc27d17
alumne <xThalach@gmail.com>
2022-03-18 16:42:15 +0100

    Blog

--- webpack.mix.js
+++ webpack.mix.js
@@ -5,12 +5,16 @@ mix.disableSuccessNotifications();
 mix.setPublicPath('source/assets/build');
 
 mix.jigsaw()
-    .js('source/_assets/js/main.js', 'js')
-    .css('source/_assets/css/main.css', 'css', [
+    .js('source/_assets/js/main.js', 'js').vue()
+    .css('source/_assets/css/main.css', 'css/main.css', [
         require('postcss-import'),
+        require('tailwindcss/nesting'),
         require('tailwindcss'),
     ])
-    .options({
-        processCssUrls: false,
+    .options({ processCssUrls: false })
+    .browserSync({
+        server: 'build_local',
+        files: ['build_local/**'],
     })
+    .sourceMaps()
     .version();
